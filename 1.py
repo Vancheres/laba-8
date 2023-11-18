a9b64489db9df7d4fe3b9cfbd6780c32908ccfdf
Vancheres <anisii840@gmail.com>
2023-11-18 05:12:44 +0500

    stoimost

--- 1.py
+++ 1.py
@@ -4,3 +4,11 @@ def assign_taxi(n, distances, rates):
 
     # Сортировка списка тарифов по возрастанию и сохранение исходных индексов
     sorted_rates = sorted(enumerate(rates), key=lambda x: x[1])
+    assignment = [0] * n
+    for i, (distance_idx, _) in enumerate(sorted_distances):
+        assignment[distance_idx] = sorted_rates[i][0] + 1  # +1 для корректного отображения номера такси
+
+    # Расчет общей стоимости
+    total_cost = sum(distances[i] * rates[assignment[i] - 1] for i in range(n))
+
+    return assignment, total_cost
