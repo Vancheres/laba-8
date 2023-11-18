422e6a034e5a02a4046fc5dbb94afec93077f283
Vancheres <anisii840@gmail.com>
2023-11-18 05:14:05 +0500

    function

--- 1.py
+++ 1.py
@@ -12,3 +12,11 @@ def assign_taxi(n, distances, rates):
     total_cost = sum(distances[i] * rates[assignment[i] - 1] for i in range(n))
 
     return assignment, total_cost
+
+n = int(input("Введите количество сотрудников: "))
+distances = list(map(int, input("Введите расстояния до домов сотрудников через пробел: ").split()))
+rates = list(map(int, input("Введите тарифы такси за километр через пробел: ").split()))
+
+assignment, total_cost = assign_taxi(n, distances, rates)
+print("Номера такси для каждого сотрудника:", ' '.join(map(str, assignment)))
+print("Общая стоимость поездок:", total_cost)
